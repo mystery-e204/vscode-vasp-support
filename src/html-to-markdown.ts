52787cbfeb634af221bf650455f1d6cd27fc5c45
Mystery <mani.e@protonmail.com>
2022-10-16 17:49:40 +0200

    Refactor even more

--- src/html-to-markdown.ts
+++ src/html-to-markdown.ts
@@ -5,23 +5,16 @@ import TurndownService from 'turndown';
 const turndownPluginGfm = require('@joplin/turndown-plugin-gfm');
 
 export class HtmlToMarkdownConverter {
-    private baseUrl: string;
     private mathConverter: MathConverter;
 	private turndownService: TurndownService;
 
-    constructor(baseUrl: string, mathConverter: MathConverter) {
-        this.baseUrl = baseUrl;
+    constructor(mathConverter: MathConverter) {
         this.mathConverter = mathConverter;
 		this.turndownService = this.createTurndownService();
     }
 
-    convert(html: string, incarTag: string): vscode.MarkdownString {
-		let markdownStr = this.turndownService.turndown(html);
-        markdownStr = this.formatMarkdown(markdownStr, incarTag);
-    
-        const markdown = new vscode.MarkdownString(markdownStr);
-        markdown.baseUri = vscode.Uri.parse(this.baseUrl);
-        return markdown;
+    convert(html: string): string {
+		return this.turndownService.turndown(html);
     }
 
     private createTurndownService(): TurndownService {
@@ -53,74 +46,6 @@ export class HtmlToMarkdownConverter {
 
 		return turndownService;
     }
-
-	private formatMarkdown(markdown: string, incarTag: string): string {
-		enum Mode {
-			value,
-			default,
-			defaultTable,
-			description,
-			moreDescription
-		}
-	
-		const lines = markdown.split("\n");
-		let outStr = `# [${incarTag}](/wiki/index.php/${incarTag} "${incarTag}")`;
-		let mode = Mode.value;
-	
-		lines.forEach(line => {
-			line = line.trim();
-	
-			switch (mode) {
-				case Mode.value:
-					if (line) {
-						const parts = line.split("=");
-						if (parts.length >= 2) {
-							outStr += `\n\n---\n\n## Value\n\n${parts[1].trim()}`;
-						} else {
-							outStr += `\n\n${line}`;
-						}
-						mode = Mode.default;
-					}
-					break;
-				case Mode.default:
-					if (line) {
-						let match = line.match(RegExp(`^Default: *(\\**${incarTag}\\** *=)? *(.*)`));
-						if (match) {
-							outStr += `\n\n---\n\n## Default\n\n${match[2]}`;
-							mode = Mode.description;
-						} else if (line.startsWith("|")) {
-							outStr += `\n\n---\n\n## Default\n\n${line}`;
-							mode = Mode.defaultTable;
-						}
-					}
-					break;
-				case Mode.defaultTable:
-					if (line.startsWith("|")) {
-						const rep = line.replace(/^\| *Default: */, "| ");
-						outStr += `\n${rep}`;
-					} else {
-						mode = Mode.description;
-					}
-					break;
-				case Mode.description:
-					if (line) {
-						const match = line.match(/^Description: *(.*)/);
-						if (match) {
-							outStr += `\n\n---\n\n## Description\n\n${match[1].trim()}`;
-						}
-						mode = Mode.moreDescription;
-					}
-					break;
-				case Mode.moreDescription:
-					outStr += `\n${line}`;
-					break;
-				default:
-					break;
-			}
-		});
-	
-		return outStr;
-	}
 }
 
 function getTextColor(): string {
---
+++ src/incar-tag.ts
@@ -0,0 +1,108 @@
+import * as vscode from "vscode";
+
+export class IncarTag {
+    name: string;
+    valueStr?: string;
+    values?: string[];
+    defaultValue?: string;
+    description?: string;
+
+    constructor(name: string, valueStr?: string, values?: string[], defaultValue?: string, description?: string) {
+        this.name = name;
+        this.valueStr = valueStr;
+        this.values = values;
+        this.defaultValue = defaultValue;
+        this.description = description;
+    }
+
+    static fromMarkdown(markdown: string, tagName: string): IncarTag {
+        enum Mode {
+            value,
+            default,
+            defaultTable,
+            description,
+            moreDescription
+        }
+    
+        const incarTag = new IncarTag(tagName);
+        const lines = markdown.split("\n");
+        let mode = Mode.value;
+    
+        lines.forEach(line => {
+            line = line.trim();
+    
+            switch (mode) {
+                case Mode.value:
+                    if (line) {
+                        const parts = line.split("=");
+                        if (parts.length >= 2) {
+                            incarTag.valueStr = parts[1].trim();
+                            incarTag.values = incarTag.valueStr.split("|").map(
+                                s => s.trim()
+                            ).filter(
+                                s => s && !(s.startsWith("[") || s.endsWith("]") || s.includes(" "))
+                            );
+                        } else {
+                            incarTag.valueStr = line;
+                        }
+                        mode = Mode.default;
+                    }
+                    break;
+                case Mode.default:
+                    if (line) {
+                        let match = line.match(RegExp(`^Default: *(\\**${incarTag}\\** *=)? *(.*)`));
+                        if (match) {
+                            incarTag.defaultValue = match[2];
+                            mode = Mode.description;
+                        } else if (line.startsWith("|")) {
+                            incarTag.defaultValue = line;
+                            mode = Mode.defaultTable;
+                        }
+                    }
+                    break;
+                case Mode.defaultTable:
+                    if (line.startsWith("|")) {
+                        const rep = line.replace(/^\| *Default: */, "| ");
+                        incarTag.defaultValue += `\n${rep}`;
+                    } else {
+                        mode = Mode.description;
+                    }
+                    break;
+                case Mode.description:
+                    if (line) {
+                        const match = line.match(/^Description: *(.*)/);
+                        if (match) {
+                            incarTag.description = match[1].trim();
+                        } else {
+                            incarTag.description = line;
+                        }
+                        mode = Mode.moreDescription;
+                    }
+                    break;
+                case Mode.moreDescription:
+                    incarTag.description += `\n${line}`;
+                    break;
+                default:
+                    break;
+            }
+        });
+    
+        return incarTag;
+    }
+
+    getHoverText(baseUrl: string): vscode.MarkdownString {
+        let markdownStr = `# [${this.name}](/wiki/index.php/${this.name} "${this.name}")`;
+    
+        function writeBlock(name: string, value?: string) {
+            return value ? `\n\n---\n\n## ${name}\n\n${value}` : "";
+        }
+    
+        markdownStr += writeBlock("Value", this.valueStr);
+        markdownStr += writeBlock("Default", this.defaultValue);
+        markdownStr += writeBlock("Description", this.description);
+    
+        const markdown = new vscode.MarkdownString(markdownStr);
+        markdown.baseUri = vscode.Uri.parse(baseUrl);
+        return markdown;
+    }
+}
