24e9ef749e72a1f3a31289c38bbf701fb51fdac1
mystery <mani.e@protonmail.com>
2022-11-06 23:28:49 +0100

    Add comment for kpoints brainstorming

---
+++ src/kpoints-parsing.ts
@@ -0,0 +1,9 @@
+//  row     explicit    regular     generalized     bandstruct  automatic
+//  1       comment     -           -               -           -
+//  2       n           0           0               n           0
+//  3       coord       mode        coord           line        auto
+//  4       k-point     subdiv      lattice         coord       length
+//  5       ...         opt shift   ...             k-point
+//  6       ...                     ...             ...
+//  7       ...                     shift           ...
+
